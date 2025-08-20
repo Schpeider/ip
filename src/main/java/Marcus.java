82304c51851bfa52934abe4fd40313608c54bf6a
Schpeider <153427968+Schpeider@users.noreply.github.com>
2025-08-20 23:03:08 +0800

    Add task list

--- src/main/java/Marcus.java
+++ src/main/java/Marcus.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Marcus {
     public static void main(String[] args) {
+        String[] taskList = new String[100];
+        int taskListSize = 0;
+
+        //Greeting Text
         System.out.println("Greetings, Marcus here!\n");
 
         String userInput = "";
@@ -13,9 +17,16 @@ public class Marcus {
 
             if (userInput.equals("bye")) {
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < taskListSize; i++) {
+                    System.out.println((i + 1) + ": " + taskList[i]);
+                }
+                System.out.print("\n");
+            } else {
+                taskList[taskListSize] = userInput;
+                taskListSize++;
+                System.out.println("added: " + userInput + "\n");
             }
-
-            System.out.println(userInput + "\n");
         }
 
         System.out.println("Mission complete! Was I helpful today?");
