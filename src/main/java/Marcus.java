18663df8602c80602549cca485a1bc6c310f35ee
Schpeider <153427968+Schpeider@users.noreply.github.com>
2025-08-20 08:31:02 +0800

    Add echo feature

--- src/main/java/Marcus.java
+++ src/main/java/Marcus.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Marcus {
     public static void main(String[] args) {
-        System.out.println("Greetings, Marcus here!\n" +
-                "What can I do for you today?\n\n" +
-                "Mission complete! Was I helpful today?");
+        System.out.println("Greetings, Marcus here!\n");
+
+        String userInput = "";
+        Scanner reader = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("What can I do for you today?: ");
+            userInput = reader.nextLine();
+
+            if (userInput.equals("bye")) {
+                break;
+            }
+
+            System.out.println(userInput + "\n");
+        }
+
+        System.out.println("Mission complete! Was I helpful today?");
     }
 }
