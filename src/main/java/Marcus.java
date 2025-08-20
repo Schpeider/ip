7a4464b49c767cd3b4942c10577111c895f044eb
Schpeider <153427968+Schpeider@users.noreply.github.com>
2025-08-20 08:04:23 +0800

    Add greeting

---
+++ src/main/java/Marcus.java
@@ -0,0 +1,7 @@
+public class Marcus {
+    public static void main(String[] args) {
+        System.out.println("Greetings, Marcus here!\n" +
+                "What can I do for you today?\n\n" +
+                "Mission complete! Was I helpful today?");
+    }
+}
